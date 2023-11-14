f68a6d7927a671977c390c028d8a5d69f2da9c6b
Raphaël <rhauteville@medinplus.com>
2023-11-14 17:29:44 +0100

    correction fichiers

--- Exos/ex7.py
+++ Exos/ex7.py
@@ -1,7 +1,7 @@
-with open('TP1.7texte.txt', 'r') as file:
+with open('ex7texte.txt', 'r') as file:
     contenu = file.read()
     mots = contenu.split()
     nombre_mots = len(mots)
 
-with open('TP1.7resultat.txt', 'w') as output_file:
+with open('ex7resultat.txt', 'w') as output_file:
     output_file.write(f"Le nombre de mots dans le fichier texte est : {nombre_mots}")
