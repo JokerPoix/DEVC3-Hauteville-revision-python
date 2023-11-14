bffe3afd44c6f8244445e8bef37f9bbba4cd7c5e
Raphaël <rhauteville@medinplus.com>
2023-11-14 15:44:28 +0100

    TP1.3 Division chaine en elements

--- TP1.3.py
+++ TP1.3.py
@@ -1,3 +1,14 @@
-def addition(chaine):
-    nombres = [float(n) for n in chaine.split()]
-    return sum(nombres)
+def evaluate_expression(expr):
+    elements = []
+    current_number = ''
+    for char in expr:
+        if char.isdigit():
+            current_number += char
+        else:
+            if current_number:
+                elements.append(current_number)
+                current_number = ''
+            if char != ' ':
+                elements.append(char)
+    if current_number:
+        elements.append(current_number)
