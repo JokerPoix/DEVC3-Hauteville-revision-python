72b636019f59c83ab8b8259eaa03633974286334
Raphaël <rhauteville@medinplus.com>
2023-11-14 17:18:18 +0100

    TP1.8 tri filtre dico

--- Exos/TP1.8.py
+++ Exos/TP1.8.py
@@ -14,3 +14,10 @@ for eleve, note in eleves.items():
         notes_par_nom[cle] = []
     notes_par_nom[cle].append(note)
 
+meilleure_note = max(note for notes in notes_par_nom.values() for note in notes)
+
+meilleurs_eleves = {eleve: notes for eleve, notes in notes_par_nom.items() if max(notes) == meilleure_note}
+
+meilleurs_eleves_tries = dict(sorted(meilleurs_eleves.items(), key=lambda x: (-max(x[1]), x[0][0], x[0][1])))
+
+
